cda8945e8c661854df883868a99f7568ce44da73
shivanian <eshivanian@hotmail.com>
2026-09-06 15:01:34 +0330

    test2 added

---
+++ test2.py
@@ -0,0 +1,7 @@
+from main import Player
+player3=Player('shivanian',44,'IKIU','IRANIAN','False')
+#print(hasattr(player3,'age'))
+#print(hasattr(player3,'Age'))
+print(getattr(player3,'nationality'))
+#print(player3.__dict__)
+#print(player3.__module__)
