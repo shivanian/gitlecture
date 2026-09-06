fc77454a65de5170dee7330637418053ebd24008
shivanian <eshivanian@hotmail.com>
2026-09-06 15:03:22 +0330

    test1 uncommented

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 from main import Player
 player2=Player('Neymar',30,'Paris Saint_Germain','Brazilian','True')
 #print(hasattr(player2,'age'))
-#print(hasattr(player2,'Age'))
-#print(getattr(player2,'nationality'))
+print(hasattr(player2,'Age'))
+print(getattr(player2,'nationality'))
 print(player2.__dict__)
 print(player2.__module__)
